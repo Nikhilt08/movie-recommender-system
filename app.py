f2a1905ea1225dbc526160731e73ff233266993c
Nikhil T <nikhilreddy6363840519@gmail.com>
2026-03-11 21:10:17 +0530

    Use secret API key for TMDB requests

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 
 
 def fetch_poster(movie_id):
-    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=enter your api key&language=en-US"
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={st.secrets['TMDB_API_KEY']}&language=en-US"
 
     data = requests.get(url)
     data = data.json()
